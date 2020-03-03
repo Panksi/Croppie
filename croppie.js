87861b781306604607565d7f11e15c4263c6150d
Karadjordje <ilija@viselio.com>
2020-03-03 12:23:16 +0100

    try to fix rotate bug

--- croppie.js
+++ croppie.js
@@ -1471,13 +1471,15 @@
 
         self.data.orientation = getExifOffset(self.data.orientation, deg);
         drawCanvas(canvas, self.elements.img, self.data.orientation);
+        self._originalImageWidth = canvas.width;
+        self._originalImageHeight = canvas.height;
         _updateCenterPoint.call(self, true);
         _updateZoomLimits.call(self);
 
         // Reverses image dimensions if the degrees of rotation is not divisible by 180.
         if ((Math.abs(deg) / 90) % 2 === 1) {
-            let oldHeight = self._originalImageHeight;
-            let oldWidth = self._originalImageWidth;
+            var oldHeight = self._originalImageHeight;
+            var oldWidth = self._originalImageWidth;
             self._originalImageWidth = oldHeight;
             self._originalImageHeight = oldWidth;
         }
